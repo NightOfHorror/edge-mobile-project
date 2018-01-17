216e4e0f7704a108b844af65b310972857f62f2d
NightOfHorror <nightbringer96@yandex.ru>
2018-01-17 23:02:37 +0700

    Last (17.01.18)

---
+++ app/src/main/java/com/example/kos/edgemobile/domains/Answers.java
@@ -0,0 +1,8 @@
+package com.example.kos.edgemobile.domains;
+
+/**
+ * Created by Константин on 17.01.2018.
+ */
+
+public class Answers {
+}
--- app/src/main/java/com/example/kos/edgemobile/retrofit/RetrofitSingleton.java
+++ app/src/main/java/com/example/kos/edgemobile/retrofit/RetrofitSingleton.java
@@ -1,20 +1,51 @@
 package com.example.kos.edgemobile.retrofit;
 
-import org.reactivestreams.Subscription;
+import android.app.Application;
+import android.util.Log;
 
-public class RetrofitSingleton {
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+import rx.Subscription;
+import rx.schedulers.Schedulers;
+
+public class AppRetrofit extends Application {
+
+    private static final String TAG = AppRetrofit.class.getSimpleName();
 
     private static Subscription subscription;
+    private static RetrofitServices retrofitServices;
 
-    private RetrofitSingleton(){
+    private Retrofit retrofit;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        retrofit = new Retrofit.Builder()
+                .baseUrl(getURL())
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        request = retrofit.create(ServerRequest.class);
     }
 
     public static void init(){
+        Log.d(TAG, "init");
 
+        RxJavaCallAdapterFactory rxAdapter = RxJavaCallAdapterFactory.createWithScheduler(Schedulers.io());
+
+        Gson gson = new GsonBuilder().create();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(url)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .addCallAdapterFactory(rxAdapter)
+                .build();
+
+        //GetModels apiService = retrofit.create(GetModels.class);
     }
 
-    public static void resetModelsObservable(){
-
-    }
 
 }
